46c534f3d4529bdeb7f40e34b67586cc80d26495
EddeCCC <eduard.schander@gmx.de>
2021-11-13 21:45:52 +0100

    add fine tuning

--- twint_scraper_v2.py
+++ twint_scraper_v2.py
@@ -7,17 +7,18 @@ META_DUMP_FILENAME = "abgeordnete_meta.json"
 
 # The CSV contains the following parties:
 # "CDU", "AfD", "B90/Die Grünen", "SPD", "FDP", "Die Linke"
-def getNamesFromCSV(csv, party):
+def getKeywordsFromCSV(csv, party):
 	rowsOfParty = csv[csv['party'] == party]
 	names = rowsOfParty[["screen_name", "full_name"]].values.flatten().tolist()
 	names.insert(0, party)
 	return names
 
-
+# Search all tweets that write about a representative or the party itself
+# For example: "Christian Lindner" or "c_lindner" or "FDP"
 def searchPoliticalTweets(csv, mentionedParty):
 	out_filename = "political_tweets_" + mentionedParty + ".csv"
-	names = getNamesFromCSV(csv, mentionedParty)[1:3]
-	print("List of names: ", names)
+	names = getKeywordsFromCSV(csv, mentionedParty)
+	print("List of keywords: ", names)
 
 	for name in names:
 
@@ -42,48 +43,7 @@ def searchPoliticalTweets(csv, mentionedParty):
 
 	return out_filename
 
-
-def searchUserTweets(username):
-	out_filename = "abgeordnete_tweets_" + username + ".json"
-
-	c = twint.Config()
-
-	c.Username = username
-	c.Store_json = True
-	c.Hide_output = True
-	c.Retweets = True
-	#c.Since = '2021-05-24'
-	#c.Until = '2021-05-31'
-	c.Output = out_filename
-
-	try:
-		twint.run.Search(c)
-	except Exception as e:
-		print(f"\n Skipped {username} because of:")
-		print(f"\n\t {e}")
-	
-	return out_filename
-
-def searchUserMeta(username):
-	out_filename = "abgeordnete_meta_" + username + ".json"
-
-	c = twint.Config()
-
-	c.Username = username
-	c.Store_json = True
-	c.Hide_output = True
-	c.Output = out_filename
-
-	
-	try:
-		twint.run.Lookup(c)
-	except Exception as e:
-		print(f"\n Skipped {username} because of:")
-		print(f"\n\t {e}")
-
-	return out_filename
-
-
+# Currently not used
 def appendPartyToJson(filename, party):
 	if not os.path.isfile(filename):
 		print(f"File {filename} is empty!")
@@ -110,18 +70,15 @@ def appendPartyToJson(filename, party):
 
 ##### START PROGRAMM #####
 
-csv_file = pandas.read_csv("Bundestag_Namen_Usernamen_Fraktion.csv", delimiter=';', engine='python')
-print("Start")
-searchPoliticalTweets(csv_file, "CDU")
+csv_file = pandas.read_csv("Bundestag_Namen_Usernamen_Fraktion.csv", delimiter=";", engine="python")
+parties = ["CDU", "SPD", "AfD", "FDP", "B90/Die Grünen", "Die Linke"]
 
-#with open('abgeordnete_usernamen_twitter.json') as rf:
-#	partymemebers = json.load(rf)
+print("Tweet extraction started")
 
-#for party in partymemebers.keys():
-#	print(f"Processing party: {party} ...")
-#	members = partymemebers[party]
-	
-#	for member in members:
-#		print(f"Current account: {member}")
-#		result_filename = searchUserTweets(member)
-#		appendPartyToJson(result_filename, party)
+for party in parties:
+	searchPoliticalTweets(csv_file, party)
+	print(f"{party} completed")
+
+print("Tweet extraction finished")
+
+
